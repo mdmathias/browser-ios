3796c516597b05ae88f8b91a40f4b1f9c3563287
Matthew Mathias <matthew.mathias@gmail.com>
2017-10-02 18:04:27 -0400

    Adds localized string
    This localized string tells the user that they are adding a new tab and leaving private mode.

--- Client/Frontend/Strings.swift
+++ Client/Frontend/Strings.swift
@@ -49,6 +49,7 @@ extension Strings {
     public static let SelectLocationBarTitle = NSLocalizedString("Select Location Bar", comment: "")
     public static let NewTabTitle = NSLocalizedString("New Tab", comment: "")
     public static let NewPrivateTabTitle = NSLocalizedString("New Private Tab", comment: "")
+    public static let NewTabLeavePrivateTitle = NSLocalizedString("New Tab (Exit Private Mode)", comment: "")
     public static let CloseAllTabsTitle = NSLocalizedString("Close All Tabs", comment: "")
     public static let CloseTabTitle = NSLocalizedString("Close Tab", comment: "")
     public static let ShowNextTabTitle = NSLocalizedString("Show Next Tab", comment: "")
