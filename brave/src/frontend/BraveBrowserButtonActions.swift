1e7062eae6805addcb0a5dd52d29b20b94ac7fd0
Matthew Mathias <matthew.mathias@gmail.com>
2017-10-02 18:30:20 -0400

    Adds a protocol with extension to handle toolbar button handlers

---
+++ brave/src/frontend/BraveBrowserButtonActions.swift
@@ -0,0 +1,35 @@
+//
+//  BraveBrowserButtonActions.swift
+//  Client
+//
+//  Created by Matthew Mathias on 10/2/17.
+//  Copyright © 2017 Mozilla. All rights reserved.
+//
+
+import UIKit
+
+/// A protocol listing methods for toolbars to implement that will
+/// attach handlers to various button actions.
+protocol BraveBrowserToolbarButtonActions {
+    /// Response to button tap where user requests new tab.
+    func respondToNewTab(action: UIAlertAction)
+    
+    /// Responds to button tap where user requests a new private tab.
+    func respondToNewPrivateTab(action: UIAlertAction)
+}
+
+// MARK: - Default Implementations
+extension BraveBrowserToolbarButtonActions {
+    func respondToNewTab(action: UIAlertAction) {
+        if PrivateBrowsing.singleton.isOn {
+            PrivateBrowsing.singleton.exit()
+            return
+        }
+        getApp().tabManager.addAdjacentTabAndSelect()
+    }
+    
+    func respondToNewPrivateTab(action: UIAlertAction) {
+        PrivateBrowsing.singleton.enter()
+        getApp().tabManager.addAdjacentTabAndSelect()
+    }
+}
